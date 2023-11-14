e929c02263f6687f045ee7097ea408f09cb10a8a
Moses-Nyagudi <nyagudi.ulridge@strathmore.edu>
2023-11-14 16:34:04 +0300

    Write your own getline function

--- main.h
+++ main.h
@@ -9,6 +9,7 @@
 #include <sys/wait.h>
 
 extern char **environ;
+#define BUFFER_SIZE 1024
 
 void execute_external_command(char **arguments);
 void execute_builtin_command(char *command);
@@ -16,5 +17,7 @@ void execute_command(char **arguments);
 char *get_location(char *command);
 char **tokenize_input(char *input);
 char *read_input(const char *prompt);
-
+char *custom_getline(void);
+void init_custom_getline(void);
+char *read_custom_getline(void);
 #endif /* MAIN_H */
--- read_user_input.c
+++ read_user_input.c
@@ -8,26 +8,6 @@
 */
 char *read_input(const char *prompt)
 {
-	char *line = NULL;
-
-	size_t line_length = 0;
-	ssize_t chars_read;
-
 	printf("%s", prompt);
-	chars_read = getline(&line, &line_length, stdin);
-
-
-	if (chars_read == -1)
-	{
-		perror("Error");
-		free(line);
-		exit(0);
-	}
-
-	if (line[chars_read - 1] == '\n')
-	{
-		line[chars_read - 1] = '\0';
-	}
-
-	return (line);
+	return (custom_getline());
 }
